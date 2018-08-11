2fc157476b5ae447fd518b147dad4621c0c4f9d8
carlesba <carlesba.mail@gmail.com>
2018-08-11 19:16:22 +0200

    MVP

--- src/App.js
+++ src/App.js
@@ -1,14 +1,24 @@
-import React, { Component } from 'react';
-import './App.css';
+import React, { Component } from 'react'
+import './App.css'
 
 class App extends Component {
-  render() {
+  state = { karma: 0 };
+  commit = value =>
+    () => this.setState(state => ({karma: state.karma + value}));
+  render () {
     return (
       <div>
         <h1>karmakarma</h1>
+        <div>
+        <button onClick={this.commit(1)}>+1</button>
+        </div>
+        <div>{this.state.karma}</div>
+        <div>
+        <button onClick={this.commit(-1)}>-1</button>
+        </div>
       </div>
-    );
+    )
   }
 }
 
-export default App;
+export default App
